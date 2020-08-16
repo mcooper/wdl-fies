ea2ed6c8ab4b72f4eab95e3d59c8c6835f6d9a66
bmueller2501 <b.mueller2501@gmail.com>
2020-08-16 23:41:00 +0200

    age/gender rescale with SSP data and figures

--- data/covars/scripts/age_gender.R
+++ data/covars/scripts/age_gender.R
@@ -62,8 +62,10 @@ library(sf)
 sf <- st_read('data/gridded_gdlpolys', 'gridded_gdlpolys')
 
 
-#sf_fies <- sf %>% filter(iso_code %in% unique(fies_raw$ISO3))
-sf_fies <- sf %>% filter(GDLcode %in% unique(fail$GDLCODE))
+sf_fies <- sf %>% filter(iso_code %in% unique(fies_raw$ISO3))
+#sf_fies <- sf %>% filter(GDLcode %in% unique(fail$GDLCODE))
+#sf_fies <- sf %>% filter(GDLcode %in% c("RUSr101", "RUSr105", "RUSr106"))
+#sf_fies <- sf %>% filter(GDLcode %in% c("RUSr107"))
 
 sf_fies.list <- split(sf_fies, f = sf_fies$GDLcode)
 sf_fies.list <- lapply(sf_fies.list, function(x) {for(i in 1:nrow(x)) {x$GDLcode_sub[i] <- paste0(x$GDLcode[i],"_",i)}; return(x)})
@@ -86,7 +88,7 @@ for(c in code) {
 age_gender <- list()
 
 # y <- 2014
-# c <- "UKRr101"
+# c <- c("RUSr101", "RUSr105", "RUSr106")
 
 for(y in 2014:2018) {
   i <- 1
@@ -134,17 +136,18 @@ age_gender2 <- read.csv('data/covars/results/age_gender_toolarge.csv')
 
 age_gender <- rbind(age_gender1, age_gender2)
 age_gender <- age_gender[order(age_gender$GDLcode),]
-#write.csv(age_gender, 'data/covars/results/age_gender_all.csv', row.names=F)
+#age_gender_fail <- age_gender_fail[order(age_gender_fail$GDLcode),]
+#write.csv(age_gender, 'data/covars/results/age_gender_withoutsubdivi.csv', row.names=F)
 
 ##------------------------------------------------------------------------------------------------------------------------------------
 
 library(wpCPR)
 library(sf)
 
-age_gender <- read.csv('data/covars/results/age_gender_all.csv')
+age_gender <- read.csv('data/covars/results/age_gender_withoutsubdivi.csv')
 
 #plot map of countries that are not working
-gdl <- sf_fies
+#gdl <- sf_fies
 mapdat <- gdl %>% 
   select(-region) %>%
   filter(iso_code %in% unique(fies_raw$ISO3))
@@ -168,10 +171,10 @@ age_gender_fail <- list()
 #c("RUSr102")
 #c("AFGr104_1", "AFGr104_2", "AFGr104_3", "AFGr104_4", "AFGr104_5")
 #fail$GDLCODE
-file.remove("data/error_age_gender.txt")
+#file.remove("data/error_age_gender.txt")
 errorFile <- file("data/error_age_gender.txt")
 
-for(y in 2014:2018) {
+for(y in 2018) {
   i <- 1
   for(c in code) {
     tryCatch({age_gender_fail[[paste0(y,"_",c)]] <- 
@@ -194,7 +197,7 @@ for(y in 2014:2018) {
   }
 }
 close(errorFile)
-save.image(file = "data/ag_save1.RData", version = NULL, ascii = FALSE, compress = !ascii, safe = TRUE)
+#save.image(file = "data/ag_save2.RData")
 
 age_gender_fail <- do.call(rbind, age_gender_fail)
 
@@ -202,19 +205,18 @@ age_gender_fail[,9:44] <- apply(age_gender_fail[,9:44], 2, as.numeric)
 ag_vars_fail <- colnames(age_gender_fail[,9:44])
 
 age_gender_fail <- age_gender_fail %>%
-  filter(year %in% c(2014, 2015)) %>%
-  group_by(GDLcode, constnt, iso_cod, country, region, shdi, GDLcd_s, year) %>%
-  summarise_at(.funs = sum, .vars = ag_vars_fail)
+  #filter(year %in% c(2014, 2015)) %>%
+  group_by(GDLcode, constnt, iso_cod, country, region, shdi, year) %>%
+  summarise_at(.funs = sum, .vars = ag_vars_fail) %>%
+  rename(constant = "constnt", iso_code = "iso_cod")
 
 #write.csv(age_gender_fail, 'data/covars/results/age_gender_subdivi1415.csv', row.names=F)
+#write.csv(age_gender_fail, 'data/covars/results/age_gender_subdivi1418.csv', row.names=F)
 
-
-
-
-
+#rus <- age_gender_fail
 
 age_gender <- read.csv('data/covars/results/age_gender_withoutsubdivi.csv')
-age_gender_fail <- read.csv('data/covars/results/age_gender_subdivi1415.csv') %>% filter(year == 2015)
+age_gender_fail <- read.csv('data/covars/results/age_gender_subdivi1418.csv')
 
 #plot map
 mapdat <- gdl %>% 
@@ -226,32 +228,26 @@ mapdat_fail <- sf_fies %>%
   filter(iso_code %in% unique(fies_raw$ISO3))
 
 countries <- ne_countries(returnclass='sf')
-ggplot() + 
-  geom_sf(data = mapdat %>% filter(iso_code %in% unique(fies_raw$ISO3)), color="black") +
-  #geom_sf(data = mapdat %>% filter(GDLCODE == "IRLr101"), fill="red") +
-  geom_sf(data = mapdat %>% filter(!GDLCODE %in% unique(age_gender$GDLcode)), fill="red") +
-  geom_sf(data = mapdat %>% filter(GDLCODE %in% unique(age_gender$GDLcode)), fill="green") +
-  
-  geom_sf(data = mapdat_fail %>% filter(!GDLcode_sub %in% unique(age_gender_fail$GDLcd_s)), fill="red") +
-  geom_sf(data = mapdat_fail %>% filter(GDLcode_sub %in% unique(age_gender_fail$GDLcd_s)), fill="green") +
-  
-  geom_sf(data=countries, color='#000000', fill=NA) + 
-  coord_sf(crs='+proj=robin') + 
-  theme_void()
+
+for(y in 2014:2018) {
+  tmp1 <- subset(age_gender, year == y)
+  tmp2 <- subset(age_gender_fail, year == y)
+  ggplot() + 
+    geom_sf(data = mapdat %>% filter(iso_code %in% unique(fies_raw$ISO3)), color="black") +
+    
+    geom_sf(data = mapdat, fill="red") +
+    geom_sf(data = mapdat %>% filter(GDLCODE %in% unique(tmp1$GDLcode)), fill="green") +
+    geom_sf(data = mapdat_fail %>% filter(GDLcode_sub %in% unique(tmp2$GDLcd_s)), fill="green") +
+    
+    geom_sf(data=countries, color='#000000', fill=NA) + 
+    coord_sf(crs='+proj=robin') + 
+    labs(title=paste0("Age/Gender Subdivision ", y),
+         fill='') + 
+    theme_void()
+  ggsave(paste0("figures/age_gender/ag_sudivi_", y, ".png"), width=10, height=5)
+}
 
 
-age_gender_fail <- read.csv('data/covars/results/age_gender_subdivi1415.csv') 
-
-age_gender_fail <- age_gender_fail %>%
-  group_by(GDLcode, constnt, iso_cod, country, region, shdi, year) %>%
-  summarise_at(.funs = sum, .vars = ag_vars_fail) %>%
-  rename(constant = "constnt", iso_code = "iso_cod")
-
-age_gender_1415 <- rbind(age_gender, age_gender_fail)
-age_gender_1415 <- age_gender_1415 %>% filter(year %in% c(2014, 2015))
-age_gender_1415 <- age_gender_1415[order(age_gender_1415$GDLcode),]
-
-write.csv(age_gender_1415, 'data/covars/results/age_gender_all1415.csv', row.names=F)
 
 # #check pop/age/gender for austria
 # age_gender$total <- apply(age_gender[,8:43], 1, sum) #check if data makes somehow sense
@@ -264,6 +260,42 @@ write.csv(age_gender_1415, 'data/covars/results/age_gender_all1415.csv', row.nam
 # #using the global age gender 1km res maps implies that we need to process 3.05gb files for both m/f and all age groups and all years
 # #not possible on my computer and i think also quite a demanding exercise for a cloud based approach
 
+age_gender <- read.csv('data/covars/results/age_gender_withoutsubdivi.csv')
+age_gender_fail <- read.csv('data/covars/results/age_gender_subdivi1418.csv')
+
+
+age_gender_fail <- age_gender_fail %>%
+  group_by(GDLcode, constant, iso_code, country, region, shdi, year) %>%
+  summarise_at(.funs = sum, .vars = ag_vars_fail)
+
+age_gender <- rbind(age_gender, age_gender_fail)
+age_gender <- age_gender[order(age_gender$GDLcode),]
+age_gender <- age_gender[order(age_gender$year),]
+#write.csv(age_gender, "data/covars/results/age_gender_all1418.csv", row.names=F)
+
+
+##------------------------------------------------------------------------------------------------------------------------------------
+
+
+age_gender <- read.csv('data/covars/results/age_gender_all1418.csv')
+
+#check pop/age/gender for austria
+age_gender$wptot <- apply(age_gender[,8:43], 1, sum) #check if data makes somehow sense
+
+age_gender <- age_gender %>% rename(GDLCODE = "GDLcode", YEAR = "year")
+age_gender <- merge(age_gender, covars %>% select(GDLCODE, YEAR, population), by = c("GDLCODE", "YEAR")) %>%
+  rename(ssptot = "population")
+
+age_gender[,8:43] <- (age_gender[,8:43]/age_gender$wptot)*age_gender$ssptot
+age_gender <- age_gender %>% select(-c("wptot", "ssptot"))
+
+write.csv(age_gender, "data/covars/results/age_gender_all1418_rescale.csv", row.names=F)
+
+
+# gdl_total <- age_gender %>% select(GDLcode, year, total) %>% rename(GDLCODE = "GDLcode", YEAR = "year")
+# gdl_total <- merge(gdl_total, covars %>% select(GDLCODE, YEAR, population), by = c("GDLCODE", "YEAR"))
+# 
+# gdl_total$what <- ((gdl_total$total-gdl_total$population)/gdl_total$total)*100
 
 
 
