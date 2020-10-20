0a9f50a02b9e2efe62a03e23bded8cfb97ed41a1
mcooper <mw.coop.r@gmail.com>
2020-10-20 00:24:06 -0400

    finish blog

---
+++ viz/Blog_Graph.R
@@ -0,0 +1,34 @@
+# setwd('~/wdl-fies'); library(ProjectTemplate); load.project(list(data_loading=T))
+
+setwd('~/wdl-fies/docs/img')
+
+options(scipen=100)
+
+#####################
+# totals time series
+###############
+
+#get totals by YEAR
+totals <- preddat %>%
+  filter(YEAR >= 2010) %>%
+  group_by(YEAR) %>%
+  summarize(mod.total=sum(fies.mod.pred * (population), na.rm=t),
+            sev.total=sum(fies.sev.pred * (population), na.rm=t)) %>%
+  gather(var, value, -YEAR) %>%
+  mutate(var = ifelse(grepl('mod', var), "Moderate And Severe", "Severe")) %>%
+  group_by(var) %>%
+  mutate(value = rollapply(value, width=3, FUN=mean, align='center', partial=TRUE))
+
+ggplot(totals) + 
+  geom_line(aes(x=YEAR, y=value, color=var), size=2) + 
+  scale_color_manual(values=c("Moderate And Severe"='#ff7f00',
+                              "Severe"='#e41a1c')) + 
+  scale_x_continuous(expand=c(0,0)) +
+  scale_y_continuous(expand=expansion(mult=c(0,0.05), add=0), 
+                     labels=function(x){prettyNum(x, big.mark=',')},
+                     limits=c(0, max(totals$value))) +
+  theme_bw() + 
+  labs(y="Number of People in Food Insecurity", title="", 
+       x='Year', color='')
+
+ggsave('Blog_Graph.png')
