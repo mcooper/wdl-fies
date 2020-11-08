e4a122523fe1aa5822a0ed3dfe0006cb6e51c5e2
mcooper <mw.coop.r@gmail.com>
2020-11-07 20:00:48 -0500

    try OOB at the country level

--- models/model_randomforest.R
+++ models/model_randomforest.R
@@ -28,17 +28,51 @@ moddat <- moddat %>%
   mutate(fies.mod = logit(fies.mod),
          fies.sev = logit(fies.sev))
 
-## mod+sev
-# tune parameters
-rf.tune.mod <- tune.rfsrc(formula = as.formula(paste("fies.mod", 
-                                                     paste(vars, collapse = "+"), sep= "~")),
-                          data = moddat,
-                          mtryStart = ncol(moddat)/2, 
-                          nodesizeTry = c(1:3),
-                          ntree = 1000,
-                          sampsize = min(nrow(moddat)*.632, max(150, nrow(moddat)^(3/4))),
-                          trace = T,
-                          dobest = T); rf.tune.mod$optimal
+prm <- expand.grid(list(node=c(1:7),
+                        mtry=c(6,5,4,3),
+                        depth=c(1:5, -1)))
+prm$ix <- 1:nrow(prm)
+
+
+#Run model under k-fold cross validation
+iso3s <- unique(moddat$ISO3)
+samp <- sample(1:10, length(iso3s), replace=T)
+for (i in 1:nrow(prm)){
+  for (s in 1:10){
+    ix <- moddat$ISO3 %in% iso3s[samp != s]
+
+    mtry <- prm$mtry[i]
+    node <- prm$node[i]
+    if(prm$depth[i] < 0){
+      depth <- prm$depth[i]
+    } else{
+      depth <- nULL
+    }
+
+    rf.mod <- rfsrc(formula = as.formula(paste("fies.mod", paste(vars, collapse = "+"), sep= "~")),
+                    data = moddat[ix, ],
+                    ntree = 1000, 
+                    mtry = mtry,
+                    nodesize = node,
+                    depth = depth,
+                    do.trace = TRUE)
+    
+    rf.sev <- rfsrc(formula = as.formula(paste("fies.sev", paste(vars, collapse = "+"), sep= "~")),
+                    data = moddat[ix, ],
+                    ntree = 1000, 
+                    mtry = mtry,
+                    nodesize = node,
+                    depth = depth,
+                    do.trace = TRUE)
+      
+    moddat$fies.mod.pred[!ix] <- inv.logit(predict(rf.mod, moddat[!ix,])$predicted)
+    moddat$fies.sev.pred[!ix] <- inv.logit(predict(rf.sev, moddat[!ix,])$predicted)
+
+    prm$sev.rsq <- cor(moddat$fies.sev.pred, moddat$fies.sev)^2
+    prm$mod.rsq <- cor(moddat$fies.mod.pred, moddat$fies.mod)^2
+  }
+}
+
 
 # model
 rf.mod <- rfsrc(formula = as.formula(paste("fies.mod", paste(vars, collapse = "+"), sep= "~")),
@@ -74,30 +108,6 @@ rf.sev <- rfsrc(formula = as.formula(paste("fies.sev", paste(vars, collapse = "+
 # predictions
 moddat$fies.sev.pred <- inv.logit(as.numeric(predict(rf.sev, moddat)$predicted))
 preddat$fies.sev.pred <- inv.logit(as.numeric(predict(rf.sev, preddat)$predicted))
-
-#Run model under k-fold cross validation
-iso3s <- unique(moddat$ISO3)
-samp <- sample(1:10, length(iso3s), replace=T)
-for (s in 1:10){
-  ix <- moddat$ISO3 %in% iso3s[samp != s]
-
-  rf.mod.cv <- rfsrc(formula = as.formula(paste("fies.mod", paste(vars, collapse = "+"), sep= "~")),
-                  data = moddat[ix, ],
-                  ntree = 1000, 
-                  mtry = rf.tune.mod$optimal[[2]],
-                  nodesize = rf.tune.mod$optimal[[1]],
-                  do.trace = TRUE)
-  
-  rf.sev.cv <- rfsrc(formula = as.formula(paste("fies.sev", paste(vars, collapse = "+"), sep= "~")),
-                  data = moddat[ix, ],
-                  ntree = 1000, 
-                  mtry = rf.tune.sev$optimal[[2]],
-                  nodesize = rf.tune.sev$optimal[[1]],
-                  do.trace = TRUE)
-
-  moddat$fies.mod.pred.cv[!ix] <- inv.logit(predict(rf.mod.cv, moddat[!ix,])$predicted)
-  moddat$fies.sev.pred.cv[!ix] <- inv.logit(predict(rf.sev.cv, moddat[!ix,])$predicted)
-}
 
 #Re-transform true outcomes
 moddat <- moddat %>%
